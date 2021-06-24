89eaf9d4088e56573d2d2d768885398ab6fec25d
Luis Pujols <luispujols@hotmail.com>
2021-06-24 17:55:51 -0400

    Added Layout for Circles and Implemented TapGestureRecognizer Logic in ViewModel

---
+++ TappableCircles/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace TappableCircles.ViewModels
+{
+    public class MainPageViewModel
+    {
+        public ICommand CircleTappedCommand { get; set; }
+
+        public MainPageViewModel()
+        {
+            CircleTappedCommand = new Command<string>(async (string circlePos) =>
+            {
+                if (Xamarin.Forms.Application.Current?.MainPage is Xamarin.Forms.Page mainPage)
+                    await MainThread.InvokeOnMainThreadAsync(async () => await mainPage.DisplayAlert("Tap Successful", $"You have succesfully tapped circle {circlePos}", "OK")).ConfigureAwait(false);
+            });
+        }
+    }
+}
--- TappableCircles/Views/MainPage.xaml.cs
+++ TappableCircles/Views/MainPage.xaml.cs
@@ -4,15 +4,17 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TappableCircles.ViewModels;
 using Xamarin.Forms;
 
-namespace TappableCircles
+namespace TappableCircles.Views
 {
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = new MainPageViewModel();
         }
     }
 }
